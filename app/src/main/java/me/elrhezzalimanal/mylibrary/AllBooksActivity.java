fd6d1255653cdd8a4b422fd2c4ab8569c6c27567
Manal El-rhezzali <manal.gzl99@gmail.com>
2020-12-31 20:43:26 +0100

    My library Application before adding the scrollView

---
+++ app/src/main/java/me/elrhezzalimanal/mylibrary/AllBooksActivity.java
@@ -0,0 +1,37 @@
+package me.elrhezzalimanal.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class AllBooksActivity extends AppCompatActivity {
+    private RecyclerView booksRecView;
+    private BookRecViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_books);
+
+        adapter = new BookRecViewAdapter(this);
+        booksRecView = findViewById(R.id.booksRecView);
+
+        booksRecView = findViewById(R.id.booksRecView);
+        booksRecView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(1,"1Q84","Haruki Murakami",1350,"https://images-na.ssl-images-amazon.com/images/I/41FdmYnaNuL._SX322_BO1,204,203,200_.jpg",
+                "A work of maddening brilliance", "Long Description"));
+        books.add(new Book(2,"You are a Badass","Jen Sincero",1350,"https://images.squarespace-cdn.com/content/5dd0ae59b4b4b35c07faf907/1580282463170-JX342II4L0N60XYA14R7/5b51ypp1C%252B97L.jpg?content-type=image%2Fjpeg",
+                "A great book", "Long Description Long Description Long Description"));
+        adapter.setBooks(books);
+        booksRecView.setAdapter(adapter);
+
+
+    }
+}
---
+++ app/src/main/java/me/elrhezzalimanal/mylibrary/BookActivity.java
@@ -0,0 +1,56 @@
+package me.elrhezzalimanal.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class BookActivity extends AppCompatActivity {
+    private TextView txtBookName, txtAuthor, txtPages, txtDescription;
+    private Button btnAddToWantToRead, btnAddToAlreadyRead, btnAddToCurrentlyReading, btnAddToFavorite;
+    private ImageView bookImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+
+        initViews();
+
+        //TODO: get the data from recycler view in here
+        Book book = new Book(1,"1Q84","Haruki Murakami",1350,"https://images-na.ssl-images-amazon.com/images/I/41FdmYnaNuL._SX322_BO1,204,203,200_.jpg",
+                "A work of maddening brilliance", "I am the long description");
+
+        setData(book);
+    }
+
+    private void setData(Book book) {
+        txtBookName.setText(book.getName());
+        txtAuthor.setText(book.getAuthor());
+        txtPages.setText(String.valueOf(book.getPages()));
+        txtDescription.setText(book.getLongDesc());
+        Glide.with(this)
+                .asBitmap().load(book.getImageUrl())
+                .into(bookImage);
+    }
+
+    private void initViews() {
+        txtAuthor = findViewById(R.id.txtAuthorName);
+        txtBookName = findViewById(R.id.txtBookName);
+        txtPages = findViewById(R.id.txtPages);
+        txtDescription = findViewById(R.id.txtDescription);
+
+        btnAddToWantToRead = findViewById(R.id.btnAddToWantToReadList);
+        btnAddToAlreadyRead = findViewById(R.id.btnAddToAlreadyReadList);
+        btnAddToCurrentlyReading = findViewById(R.id.btnAddToCurrentlyReading);
+        btnAddToFavorite = findViewById(R.id.btnAddToFavorite);
+        bookImage = findViewById(R.id.imgBook);
+
+    }
+
+
+}
