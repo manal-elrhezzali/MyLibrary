b37f9119d55e1c29b7b4151a48fc72bedd98be32
Manal El-rhezzali <manal.gzl99@gmail.com>
2021-01-01 17:47:13 +0100

    added showing website in a WebView functionality

--- app/src/main/java/me/elrhezzalimanal/mylibrary/MainActivity.java
+++ app/src/main/java/me/elrhezzalimanal/mylibrary/MainActivity.java
@@ -1,11 +1,14 @@
 package me.elrhezzalimanal.mylibrary;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -55,6 +58,33 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, FavoriteBooksActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(R.string.app_name);
+                builder.setMessage("Designed and Developed by Manal, feel free to check my blogs at http://elrhezzali-manal.me/");
+                builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.setPositiveButton("Visit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                       // navigates the user to the website google.com
+                        Intent intent = new Intent(MainActivity.this, WebsiteActivity.class);
+                        intent.putExtra("url","https://www.google.com/search?sxsrf=ALeKk03vf6KaDIm0Nz37wl_4WyBrGmQX7A%3A1609519480189&source=hp&ei=eFHvX9rXCdGaa7K-vKgC&q=http%3A%2F%2Felrhezzali-manal.me%2F&oq=http%3A%2F%2Felrhezzali-manal.me%2F&gs_lcp=CgZwc3ktYWIQAzoHCCMQ6gIQJ1DWCVjWCWCbEmgAcAB4AIAB3gKIAZcEkgEHMC4xLjAuMZgBAKABAqABAaoBB2d3cy13aXqwAQo&sclient=psy-ab&ved=0ahUKEwja8_6ml_vtAhVRzRoKHTIfDyUQ4dUDCAc&uact=5");
+                        startActivity(intent);
+
+                    }
+                });
+
+                builder.create().show();
             }
         });
 
